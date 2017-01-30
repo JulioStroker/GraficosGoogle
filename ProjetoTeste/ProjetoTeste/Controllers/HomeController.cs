ab9f4738888c7ccdebbdff4bc251a8b5aba01bb1
Júlio Pinheiro <Jlio Pinheiro>
2017-01-29 23:33:30 -0200

    Adicionado Projeto com Gráficos do Google e MVC

---
+++ ProjetoTeste/ProjetoTeste/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using ProjetoTeste.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace ProjetoTeste.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Publico obj = new Publico()
+            {
+                QtdFeminino = 30,
+                QtdMasculino = 50,
+                QtdNaoInformado = 10,
+                QtdTotal = 30 + 50 + 10
+            };
+
+            return View(obj);
+        }
+    }
+}
---
+++ ProjetoTeste/ProjetoTeste/Models/Publico.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoTeste.Models
+{
+    public class Publico
+    {
+        public int QtdMasculino { get; set; }
+        public int QtdFeminino { get; set; }
+        public int QtdNaoInformado { get; set; }
+        public int QtdTotal { get; set; }
+
+    }
+
+
+}
